ee94435cdc4bdffb048283120a0eb20ffbe61822
zofiamilczarek <79087756+zofiamilczarek@users.noreply.github.com>
2022-02-17 16:07:46 +0100

    textures updated
    now 1st 4 vals are coordiantes and the last 2 are parameters we can use

--- src/main/java/Controller/Texture.java
+++ src/main/java/Controller/Texture.java
@@ -2,17 +2,14 @@ package Controller;
 
 import java.util.ArrayList;
 
-public class Texture extends MapObject {
+public class Texture extends Rectangle {
 
+    private int parameter1,parameter2;
 
     public Texture(int x1, int x2, int x3, int x4, int x5, int x6){
-        super();
-        super.coords.add(x1);
-        super.coords.add(x2);
-        super.coords.add(x3);
-        super.coords.add(x4);
-        super.coords.add(x5);
-        super.coords.add(x6);
+        super(x1,x2,x3,x4);
+        this.parameter1 = x5;
+        this.parameter2 = x6;
     }
 
 }
