7f47dfd617cbc39633cf0c4ddca3eb53caf75c4e
TiphanieBentDke <56296095+TiphanieBentDke@users.noreply.github.com>
2022-02-23 12:18:28 +0100

    MAP highlevel

--- src/main/java/Agents/Agent.java
+++ src/main/java/Agents/Agent.java
@@ -1,4 +1,4 @@
-package Agents;
+:package Agents;
 
 import Controller.Variables;
 import Controller.Vector;
@@ -30,78 +30,10 @@ public class Agent  {
         for(int i = 0; i < 4 ; i++) {
             spawning[i] = variables.getSpawnAreaGuards()[i];
         }
-        convertPosition(this.getTeam());// setting agent inside the Map
+        // convertPosition(this.getTeam());// setting agent inside the Map
         updateMap();//update the map With the agent knwoledge
     }
 
-    public void convertPosition([Agent] team){
-        int teamSize = team.size();
-        int x1 = team[0].getAgentSpawning()[0];
-        int y1 = team[0].getAgentSpawning()[1];
-        int x2 = team[0].getAgentSpawning()[2];
-        int y2 = team[0].getAgentSpawning()[3];
-
-        int width = x1- x2;
-        int height = y1 - y2;
-        int counter = 0;
-//CASE 1:  available position inferior or equal to number of guards
-        if(teamSize => (width * height)){
-            while( counter  < teamSize ){
-                for(int i = x1; i < x2 + 1; i ++){
-                    for(int j = y1; j < y2; j ++){
-                        //   map[getAgentSpawning()[0]][i] = 0 ; guards code
-                        team[i].setAgentPositionX() = i;
-                        team[i].setAgentPositionY() = j;
-                        counter++;
-                    }
-                }
-            }
-        }//END CASE 1
-
-        //CASE 2: available position superior to number of guards
-        int decisionRatio= 30/100;
-
-        if(teamSize < decisionRatio * (width * height)){
-            while( counter  < teamSize ){
-                // top line
-                for(int i = x1; i < x2 + 1; i ++){
-                    if (counter  < teamSize){
-                        //   map[getAgentSpawning()[0]][i] = 0 ; guards code
-                        team[counter].setAgentPositionX() = i;
-                        team[counter].setAgentPositionY() = y2;
-                        counter++;
-                    }
-                }
-                // bottom line
-                for(int i = x1; i < x2 + 1; i ++){
-                    if (counter  < teamSize){
-                        //   map[getAgentSpawning()[0]][i] = 0 ; guards code
-                        team[counter].setAgentPositionX() = i;
-                        team[counter].setAgentPositionY() = y1;
-                        counter++;
-                    }
-                }
-                // start column
-                for(int j = y1; j < y2 + 1; j ++){
-                    if (counter  < teamSize){
-//   map[getAgentSpawning()[0]][i] = 0 ; guards code
-                        team[counter].setAgentPositionX() = x1;
-                        team[counter].setAgentPositionY() = j;
-                        counter++;
-                    }
-                }
-                // end column
-                for(int j = y1; j < y2 + 1; j ++){
-                    if (counter  < teamSize){
-                        //   map[getAgentSpawning()[0]][i] = 0 ; guards code
-                        team[counter].setAgentPositionX() = x2;
-                        team[counter].setAgentPositionY() = j;
-                        counter++;
-                    }
-                }
-            }
-        }//END CASE 2
-    }//END convertPosition
 
     public int[] getAgentSpawning(){return spawning;}
     public void setAgentPositionX(int agentPositionX){this.agentPositionX = agentPositionX;}
