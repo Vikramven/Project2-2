d83faf214d3f290c286a9bed4a62fec3ca26b54c
Zaker <zaker.omargeel@gmail.com>
2022-02-24 00:08:48 +0100

    Parser update

--- src/main/java/Visuals/Controller/DefaultVariables.java
+++ src/main/java/Visuals/Controller/DefaultVariables.java
@@ -30,6 +30,67 @@ public class DefaultVariables {
     private Vector2f spawnIntruder;
     private Vector2f spawnGuard;
 
+    public DefaultVariables() {
+    }
+
+    public void setVariables(String[] stringOfAllVariables){
+
+
+        this.name = stringOfAllVariables[0];
+        this.gameFile = stringOfAllVariables[1];
+        this.gameMode = Integer.parseInt(stringOfAllVariables[2]);
+        this.height = Integer.parseInt(stringOfAllVariables[3]);
+        this.width = Integer.parseInt(stringOfAllVariables[4]);
+        this.scaling = Float.parseFloat(stringOfAllVariables[5]);
+        this.numGuards = Integer.parseInt(stringOfAllVariables[6]);
+        this.numIntruders = Integer.parseInt(stringOfAllVariables[7]);
+        this.baseSpeedIntruder = Float.parseFloat(stringOfAllVariables[8]);
+        this.sprintSpeedIntruder = Float.parseFloat(stringOfAllVariables[9]);
+        this.baseSpeedGuard = Float.parseFloat(stringOfAllVariables[10]);
+        this.timeStep = Float.parseFloat(stringOfAllVariables[11]);
+
+        String[] targetArea = stringOfAllVariables[12].split(" ");
+        String[] spawnAreaIntruders = stringOfAllVariables[13].split(" ");
+        String[] spawnAreaGuards = stringOfAllVariables[14].split(" ");
+        String[] wall1 = stringOfAllVariables[15].split(" ");
+        String[] wall2 = stringOfAllVariables[16].split(" ");
+        String[] wall3 = stringOfAllVariables[17].split(" ");
+        String[] wall4 = stringOfAllVariables[18].split(" ");
+        String[] wall5 = stringOfAllVariables[19].split(" ");
+
+        this.targetArea = new Vector4f(Float.parseFloat(targetArea[0]), Float.parseFloat(targetArea[1]), Float.parseFloat(targetArea[2]), Float.parseFloat(targetArea[3]));
+        this.spawnAreaIntruders = new Vector4f(Float.parseFloat(spawnAreaIntruders[0]), Float.parseFloat(spawnAreaIntruders[1]), Float.parseFloat(spawnAreaIntruders[2]), Float.parseFloat(spawnAreaIntruders[3]));
+        this.spawnAreaGuards = new Vector4f(Float.parseFloat(spawnAreaGuards[0]), Float.parseFloat(spawnAreaGuards[1]), Float.parseFloat(spawnAreaGuards[2]), Float.parseFloat(spawnAreaGuards[3]));
+        this.wall1 = new Vector4f(Float.parseFloat(wall1[0]), Float.parseFloat(wall1[1]), Float.parseFloat(wall1[2]), Float.parseFloat(wall1[3]));
+        this.wall2 = new Vector4f(Float.parseFloat(wall2[0]), Float.parseFloat(wall2[1]), Float.parseFloat(wall2[2]), Float.parseFloat(wall2[3]));
+        this.wall3 = new Vector4f(Float.parseFloat(wall3[0]), Float.parseFloat(wall3[1]), Float.parseFloat(wall3[2]), Float.parseFloat(wall3[3]));
+        this.wall4 = new Vector4f(Float.parseFloat(wall4[0]), Float.parseFloat(wall4[1]), Float.parseFloat(wall4[2]), Float.parseFloat(wall4[3]));
+        this.wall5 = new Vector4f(Float.parseFloat(wall5[0]), Float.parseFloat(wall5[1]), Float.parseFloat(wall5[2]), Float.parseFloat(wall5[3]));
+
+        // Random SpawnPoints
+        // intruder
+        Random random = new Random();
+
+        float randomXIntruder = Float.parseFloat(spawnAreaIntruders[0]) + random.nextInt((int) Float.parseFloat(spawnAreaIntruders[2]));
+        float randomYIntruder = Float.parseFloat(spawnAreaIntruders[1]) + random.nextInt((int) Float.parseFloat(spawnAreaIntruders[3]));
+
+        this.spawnIntruder = new Vector2f(randomXIntruder,randomYIntruder);
+
+        //guard
+        float randomXGuard = Float.parseFloat(spawnAreaGuards[0]) + random.nextInt((int) Float.parseFloat(spawnAreaGuards[2]));
+        float randomYGuard = Float.parseFloat(spawnAreaGuards[1]) + random.nextInt((int) Float.parseFloat(spawnAreaGuards[3]));
+
+        this.spawnGuard = new Vector2f(randomXGuard,randomYGuard);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Book [name=" + name + "]";
+    }
+
+    // getters n setters
+
     public String getName() {
         return name;
     }
@@ -116,69 +177,6 @@ public class DefaultVariables {
 
     public Vector4f getWall5() {
         return wall5;
-    }
-
-
-    public DefaultVariables() {
-    }
-
-    public void setVariables(String[] stringOfAllVariables){
-
-
-
-        this.name = stringOfAllVariables[0];
-        this.gameFile = stringOfAllVariables[1];
-        this.gameMode = Integer.parseInt(stringOfAllVariables[2]);
-        this.height = Integer.parseInt(stringOfAllVariables[3]);
-        this.width = Integer.parseInt(stringOfAllVariables[4]);
-        this.scaling = Float.parseFloat(stringOfAllVariables[5]);
-        this.numGuards = Integer.parseInt(stringOfAllVariables[6]);
-        this.numIntruders = Integer.parseInt(stringOfAllVariables[7]);
-        this.baseSpeedIntruder = Float.parseFloat(stringOfAllVariables[8]);
-        this.sprintSpeedIntruder = Float.parseFloat(stringOfAllVariables[9]);
-        this.baseSpeedGuard = Float.parseFloat(stringOfAllVariables[10]);
-        this.timeStep = Float.parseFloat(stringOfAllVariables[11]);
-
-        String[] targetArea = stringOfAllVariables[12].split(" ");
-        String[] spawnAreaIntruders = stringOfAllVariables[13].split(" ");
-        String[] spawnAreaGuards = stringOfAllVariables[14].split(" ");
-        String[] wall1 = stringOfAllVariables[15].split(" ");
-        String[] wall2 = stringOfAllVariables[16].split(" ");
-        String[] wall3 = stringOfAllVariables[17].split(" ");
-        String[] wall4 = stringOfAllVariables[18].split(" ");
-        String[] wall5 = stringOfAllVariables[19].split(" ");
-
-        this.targetArea = new Vector4f(Float.parseFloat(targetArea[0]), Float.parseFloat(targetArea[1]), Float.parseFloat(targetArea[2]), Float.parseFloat(targetArea[3]));
-        this.spawnAreaIntruders = new Vector4f(Float.parseFloat(spawnAreaIntruders[0]), Float.parseFloat(spawnAreaIntruders[1]), Float.parseFloat(spawnAreaIntruders[2]), Float.parseFloat(spawnAreaIntruders[3]));
-        this.spawnAreaGuards = new Vector4f(Float.parseFloat(spawnAreaGuards[0]), Float.parseFloat(spawnAreaGuards[1]), Float.parseFloat(spawnAreaGuards[2]), Float.parseFloat(spawnAreaGuards[3]));
-        this.wall1 = new Vector4f(Float.parseFloat(wall1[0]), Float.parseFloat(wall1[1]), Float.parseFloat(wall1[2]), Float.parseFloat(wall1[3]));
-        this.wall2 = new Vector4f(Float.parseFloat(wall2[0]), Float.parseFloat(wall2[1]), Float.parseFloat(wall2[2]), Float.parseFloat(wall2[3]));
-        this.wall3 = new Vector4f(Float.parseFloat(wall3[0]), Float.parseFloat(wall3[1]), Float.parseFloat(wall3[2]), Float.parseFloat(wall3[3]));
-        this.wall4 = new Vector4f(Float.parseFloat(wall4[0]), Float.parseFloat(wall4[1]), Float.parseFloat(wall4[2]), Float.parseFloat(wall4[3]));
-        this.wall5 = new Vector4f(Float.parseFloat(wall5[0]), Float.parseFloat(wall5[1]), Float.parseFloat(wall5[2]), Float.parseFloat(wall5[3]));
-
-        // Random SpawnPoints
-        // intruder
-        Random random = new Random();
-
-        float randomXIntruder = Float.parseFloat(spawnAreaIntruders[0]) + random.nextInt((int) Float.parseFloat(spawnAreaIntruders[2]));
-        float randomYIntruder = Float.parseFloat(spawnAreaIntruders[1]) + random.nextInt((int) Float.parseFloat(spawnAreaIntruders[3]));
-
-        this.spawnIntruder = new Vector2f(randomXIntruder,randomYIntruder);
-
-        //guard
-        float randomXGuard = Float.parseFloat(spawnAreaGuards[0]) + random.nextInt((int) Float.parseFloat(spawnAreaGuards[2]));
-        float randomYGuard = Float.parseFloat(spawnAreaGuards[1]) + random.nextInt((int) Float.parseFloat(spawnAreaGuards[3]));
-
-        this.spawnGuard = new Vector2f(randomXGuard,randomYGuard);
-
-
-
-    }
-
-    @Override
-    public String toString() {
-        return "Book [name=" + name + "]";
     }
 
 }
